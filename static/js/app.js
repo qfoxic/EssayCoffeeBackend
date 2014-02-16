9b3e9594447c96c383e4340c9b7f7f2ef087e2d5
andrew <andrew@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-02-16 17:36:59 +0000

    new task form layout remaster

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,12 @@
 $( document ).ready( function( ) {
-
+    $( '.datetimepicker' ).datetimepicker( {
+        language: 'ru',
+        useSeconds: false,
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-arrow-up",
+            down: "fa fa-arrow-down"
+        }
+    } );
 } );
