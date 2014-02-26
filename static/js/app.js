21d3d3820271b8cb59695f2181723b3cd210e4c3
andrew <andrew@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-02-26 21:16:02 +0000

    removed unused scripts task edit form face lift

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,14 @@
 $( document ).ready( function( ) {
-    $( '.datetimepicker' ).datetimepicker( {
-        language: 'ru',
-        format: 'YYYY-MM-DD HH:mm',
-        useSeconds: false,
-        icons: {
-            time: "fa fa-clock-o",
-            date: "fa fa-calendar",
-            up: "fa fa-arrow-up",
-            down: "fa fa-arrow-down"
-        }
-    } );
+        // $( '.datetimepicker' ).datetimepicker( {
+        // language: 'ru',
+        // format: 'YYYY-MM-DD HH:mm',
+        // useSeconds: false,
+        // icons: {
+            // time: "fa fa-clock-o",
+            // date: "fa fa-calendar",
+            // up: "fa fa-arrow-up",
+            // down: "fa fa-arrow-down"
+        // }
+    // } );
+    $( '.upload-file' ).bootstrapFileInput( );
 } );
