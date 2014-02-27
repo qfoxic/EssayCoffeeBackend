e1931d9ff6b2efb1d3498788af544abe596c8701
andrew <andrew@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-02-27 11:03:22 +0000

    user profile form face-lift

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,17 @@
 $( document ).ready( function( ) {
-        // $( '.datetimepicker' ).datetimepicker( {
-        // language: 'ru',
-        // format: 'YYYY-MM-DD HH:mm',
-        // useSeconds: false,
-        // icons: {
-            // time: "fa fa-clock-o",
-            // date: "fa fa-calendar",
-            // up: "fa fa-arrow-up",
-            // down: "fa fa-arrow-down"
-        // }
+    // $( '.datetimepicker' ).datetimepicker( {
+    // language: 'ru',
+    // format: 'YYYY-MM-DD HH:mm',
+    // useSeconds: false,
+    // icons: {
+    // time: "fa fa-clock-o",
+    // date: "fa fa-calendar",
+    // up: "fa fa-arrow-up",
+    // down: "fa fa-arrow-down"
+    // }
     // } );
     $( '.upload-file' ).bootstrapFileInput( );
+    $( '.has-error' ).tooltip( {
+        container: 'body',
+    } );
 } );
