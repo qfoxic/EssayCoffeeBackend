7d231d38609d380f0cae80a1bfdfedee358e72db
vpaslav <vpaslav@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-03-25 15:03:03 +0000

    Fixed incompatible pythons code

--- general/models.py
+++ general/models.py
@@ -1,5 +1,5 @@
 import os
-import time
+import datetime 
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -72,10 +72,12 @@ class Task(models.Model):
   get_style = lambda self: co.STYLES_DICT.get(self.style) 
   
   def days_expired(self):
-    return (self.urgency-(time.time()-int(self.created.strftime('%s'))))/86400
+    delta = datetime.datetime.today() - self.created
+    return (self.urgency-delta.seconds())/86400
 
   def hours_expired(self):
-    return (self.urgency-(time.time()-int(self.created.strftime('%s'))))/3600
+    delta = datetime.datetime.today() - self.created
+    return (self.urgency-delta.seconds())/3600
    
   @classmethod 
   def get_finished_tasks(cls, count_only, **kwargs):
