0c9c13e593b991a3192e9c9b5a8f07f9db84e20d
andrew <andrew@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-04-04 09:42:10 +0000

    fixed draft icon appearence

--- userprofile/templatetags/render_status.py
+++ userprofile/templatetags/render_status.py
@@ -1,19 +1,22 @@
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
+import constants
  
 register = template.Library()
  
 @register.filter(name='render_status')
 def render_status(value):
-  options = {   0 : _unprocessed,
-                1 : _processing,
-                2 : _rejected,
-                3 : _completed,
-                4 : _unprocessed,
-                5 : _suspicious,
-                6 : _draft,
+
+  options = {   
+    constants.UNPROCESSED : _unprocessed,
+    constants.PROCESSING  : _processing,
+    constants.REJECTED    : _rejected,
+    constants.COMPLETED   : _completed,
+    constants.DRAFT       : _draft,
+    constants.SUSPICIOUS  : _suspicious,
   }  
+
   return  mark_safe(options[value]())
 
 render_status.is_safe = False  
@@ -34,4 +37,4 @@ def _rejected():
   return '<i class="fa fa-times-circle has_tooltip" style="font-size:1.5em;color:#A04040" data-placement="right" title="Rejected"></i>'
 
 def _draft():
-  return '<i class="fa fa-file-o has_tooltip" style="font-size:1.5em;color:#2080D0" data-placement="right" title="Draft"></i>'
+  return '<i class="fa fa-file-o has_tooltip" style="font-size:1.5em;color:#9A9A9A" data-placement="right" title="Draft"></i>'
