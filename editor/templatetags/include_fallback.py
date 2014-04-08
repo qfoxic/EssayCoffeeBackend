ac630d6b7947ff43979f48bfe5daa5e62d798cd8
vpaslav <vpaslav@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-04-08 15:16:41 +0000

    Removed extra template tags

--- editor/templatetags/include_fallback.py
+++
@@ -1,7 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.simple_tag(takes_context=True)
-def include_fallback(context, *template_choices):
-    t = template.loader.select_template(template_choices)
-    return t.render(context)
