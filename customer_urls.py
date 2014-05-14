2a02a34ccfa50bee8eb5bad0f765c8edc8a31446
vpaslav <vpaslav@410dc90c-7c34-11e3-9520-45e418359d2a>
2014-05-14 03:53:18 +0000

    Fixed bug with visible task details for a customers

--- customer_urls.py
+++ customer_urls.py
@@ -33,7 +33,7 @@ task_list = lambda request: login_required(
     TaskIndexView.as_view(module_name='customer', action_label='my orders',
                           queryset=Task.get_all_tasks(0, **{'owner__id': request.user.id})),
     login_url=reverse_lazy('login'))(request)
-task_details = login_required(DetailTaskView.as_view(module_name='customer'),
+task_details = login_required(DetailTaskView.as_view(module_name='customer', owner_required=True),
                               login_url=reverse_lazy('login'))
 task_new = login_required(
   CreateTaskView.as_view(module_name='customer'),
