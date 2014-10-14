ff4894b8b3d47375c38b3c78f00a768d8151d416
Volodymyr Paslavskyy <workforum@ukr.net>
2014-10-14 20:41:25 +0000

    Fixed ftp settings

--- writer_settings.py
+++ writer_settings.py
@@ -68,8 +68,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.request",
 )
 
-FTP_DATA = ('127.0.0.1', 2121, 'ftp', 'q1w2e3r4t5y6')
-
+FTP_DATA = ('127.0.0.1', 21, 'ftpuser', '(a@4=4ab=b@4)')
 DEFAULT_FILE_STORAGE = (
     'ftpstorage.storage.FTPStorage'
 )
